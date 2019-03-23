f3f055f39af3e5d464424691223dd9cf6da804dd
andiecodes <morganh@gmx.li>
2019-03-23 14:05:02 +0100

    WIP

--- fm.js
+++ fm.js
@@ -1,20 +1,6 @@
-var element = document.getElementsByName('loginfmt')[0].value;
-if (element != null && element.value == '') {
-  console.log('Mail is not set, try to insert current user ');
+console.log('code runs');
+var element = document.getElementsByName('loginfmt');
+if (element[0].value = '0') {
+  element[0].value = 'currentusermail';
 }
-
-function logTabs(windowInfo) {
-  for (let tabInfo of windowInfo.tabs) {
-    console.log(tabInfo.url);
-  }
-}
-
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
-
-browser.browserAction.onClicked.addListener((tab) => {
-  var getting = browser.windows.getCurrent({populate: true});
-  getting.then(logTabs, onError);
-});
 
