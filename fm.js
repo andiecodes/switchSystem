bd1c7cf4592b3486f4c56689b66d664115991b72
andiecodes <morganh@gmx.li>
2019-03-23 11:22:09 +0100

    fix syntax error

--- fm.js
+++ fm.js
@@ -1,12 +1,12 @@
-function(win, doc, $, undefined) {
+(function(win, doc, $, undefined) {
     'use strict';
     
     // Don't run if jQuery isn't loaded
     if (typeof window.jQuery === 'undefined') {
-      console.log('jquerry undefined');
-	  return;
+		console.log('jquerry undefined');
+      return;
     }
     
     // Our code will go here.
 
-  }(window, window.document, window.jQuery);
+  }(window, window.document, window.jQuery));
