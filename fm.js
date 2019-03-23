608e6375472b64383bd3bb871c57ec62e99dadd0
andiecodes <morganh@gmx.li>
2019-03-23 14:28:40 +0100

    WIP

--- fm.js
+++ fm.js
@@ -1,6 +1,22 @@
 console.log('code runs');
-var element = document.getElementsByName('loginfmt');
-if (element[0].value = '0') {
-  element[0].value = 'currentusermail';
+
+function getUserData(){    
+          try { 	   
+            var objUsr = JSON.parse(localStorage.getItem('mx.shell.userInfoCache'));  	   
+            var usrname = (objUsr.FirstName + '.' +  objUsr.LastName + '@de.rhenus.com'); 
+            var element = document.getElementsByName('loginfmt');
+            if (element[0].value = '0') {
+              element[0].value = usrname;
+            }
+          }    
+          catch(err) { 			  
+            console.log('No user data found in local storage. Error: ' + err.message); 			 }; 
+          }; 
+
+
+if(document.readyState === 'complete') 
+{ 	  	  
+  getUserData(); 	 
 }
+
 
