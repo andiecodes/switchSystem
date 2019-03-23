79ca5607c952c6072ce1f6b54dddbe16cb907df1
andiecodes <morganh@gmx.li>
2019-03-23 10:25:56 +0100

    bookmark test
    test if file is reachable by js code snippet in bookmark

---
+++ fm.js
@@ -0,0 +1,11 @@
+(function(win, doc, $, undefined) {
+    'use strict';
+    
+    // Don't run if jQuery isn't loaded
+    if (typeof window.jQuery === 'undefined') {
+      return;
+    }
+    
+    // Our code will go here.
+
+  }(window, window.document, window.jQuery));
