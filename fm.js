5c77f4a9882bdd2c749093b752c9437e1144e643
andiecodes <morganh@gmx.li>
2019-03-23 13:02:19 +0100

    WIP

--- fm.js
+++ fm.js
@@ -1,1 +1,20 @@
-console.log('code runs');
+var element = document.getElementsByName('loginfmt')[0].value;
+if (element != null && element.value == '') {
+  console.log('Mail is not set, try to insert current user ');
+}
+
+function logTabs(windowInfo) {
+  for (let tabInfo of windowInfo.tabs) {
+    console.log(tabInfo.url);
+  }
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+browser.browserAction.onClicked.addListener((tab) => {
+  var getting = browser.windows.getCurrent({populate: true});
+  getting.then(logTabs, onError);
+});
+
