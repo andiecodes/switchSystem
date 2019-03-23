37807434c319b5c9c4ee08e5a5a21dce50d71097
andiecodes <morganh@gmx.li>
2019-03-23 11:47:38 +0100

    simple console.log

--- fm.js
+++ fm.js
@@ -1,13 +1,1 @@
-(function(win, doc, $, undefined) {
-    'use strict';
-    
-    // Don't run if jQuery isn't loaded
-    if (typeof window.jQuery === 'undefined') {
-		console.log('jquery undefined');
-	    //test
-      return;
-    }
-    
-    // Our code will go here.
-
-  }(window, window.document, window.jQuery));
+console.log('code runs');
