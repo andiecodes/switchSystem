ed9358da92929dbf73738e53fadd22c1fbf5747b
andiecodes <morganh@gmx.li>
2019-03-23 15:15:33 +0100

    wip

--- iM.js
+++ iM.js
@@ -1,18 +1,15 @@
-console.log('code runs');
-
-function getUserData(){    
+function switchSystem(){    
           try { 	   
-            var element = document.getElementsByName('loginfmt');
-            if (element[0].value = '0') {
-              element[0].value = 'andreas.stoecker@de.rhenus.com';
-            }
-          }    
+            
+
+            
+              }    
           catch(err) { 			  
             console.log('Error: ' + err.message); 			 }; 
           }; 
 if(document.readyState === 'complete') 
 { 	  	  
-  getUserData(); 	 
+  switchSystem(); 	 
 }
 
 
